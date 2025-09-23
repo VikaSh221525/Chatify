075293e8ad47c047c31455fde71b666b5cc49c33
VikaSh221525 <vikassharma221525@gmail.com>
2025-09-23 23:58:51 +0530

    Arcjet

---
+++ backend/src/middlewares/arcjet.middleware.js
@@ -0,0 +1,32 @@
+import aj from "../lib/arcjet.js";
+import { isSpoofedBot } from "@arcjet/inspect";
+
+export const arcjetProtection = async (req, res, next) => {
+    try {
+        const decision = await aj.protect(req, res);
+        if (decision.isDenied()) {
+            if (decision.reason.isRateLimit()) {
+                return res.status(429).json({ message: "Too many requests! Rate limit exceeded. Please try again later." });
+            } else if (decision.reason.isBot()) {
+                return res.status(403).json({ message: "Bot access denied" });
+            } else {
+                return res.status(403).json({
+                    message: "Access denied by security policy"
+                });
+            }
+        }
+
+        // check for spoofed bot
+        if(decision.results.some(isSpoofedBot)){
+            return res.status(403).json({
+                error: "Spoofed bot access denied", 
+                message: "Spoofed bot access denied" 
+            });
+        }
+
+        next();
+    } catch (error) {
+        console.log("Arcjet inspecting error", error);
+        next(error);
+    }
+}
