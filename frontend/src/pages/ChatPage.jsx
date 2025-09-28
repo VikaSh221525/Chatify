d04ab0794213b1befe4c87fb79dcd9162430ece0
VikaSh221525 <vikassharma221525@gmail.com>
2025-09-28 16:37:17 +0530

    ChatPageSetup

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import { useAuthStore } from '../store/useAuthStore';
+import { useChatStore } from '../store/useChatStore';
+import ActiveTabSwitch from '../components/ActiveTabSwitch';
+import ChatList from '../components/ChatList';
+import ContactList from '../components/ContactList';
+import ProfileHeader from '../components/ProfileHeader';
+import ChatContainer from '../components/ChatContainer';
+import NoConversationPlaceHolder from '../components/NoConversationPlaceHolder';
 
 const ChatPage = () => {
     const { logout } = useAuthStore();
+    const { activeTab, selectedUser } = useChatStore();
+
     return (
-        <div className='z-10'>
-            <button onClick={logout} className='btn btn-outline btn-accent btn-wide'>
-                Logout
-            </button>
+        <div className='box relative w-full max-w-6xl h-[600px] flex'>
+            {/* Left side */}
+            <div className='w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col'>
+                <ProfileHeader />
+                <ActiveTabSwitch />
+                <div className='flex-1 overflow-y-auto p-4 space-y-4'>
+                    {activeTab === "chats" ? (
+                        <ChatList />
+                    ) : (
+                        <ContactList />
+                    )}
+                </div>
+            </div>
+
+            {/* Right Side */}
+            <div className='flex-1 flex flex-col backdrop-blur-sm'>
+                {selectedUser ? <ChatContainer/> : <NoConversationPlaceHolder/>}
+
+            </div>
+
         </div>
     )
 }
---
+++ frontend/src/store/useChatStore.js
@@ -0,0 +1,50 @@
+import { create } from "zustand";
+import { toast } from "react-hot-toast";
+import { axiosInstance } from "../lib/axios";
+
+export const useChatStore = create((set, get) => ({
+    allContacts: [],
+    chats: [],
+    messages: [],
+
+    activeTab: "chats",
+    selectedUser: null,
+
+    isUsersLoading: false,
+    isMessagesLoading: false,
+    isSoundEnabled: localStorage.getItem("isSoundEnabled") === "true",
+
+    toggleSound: () => {
+        localStorage.setItem("isSoundEnabled", !get().isSoundEnabled);
+        set({isSoundEnabled: !get().isSoundEnabled});
+    },
+
+    seActiveTab: (tab) => set({activeTab: tab}),
+    setSelectedUser: (user) => set({selectedUser: user}),
+
+    getAllContacts: async() => {
+        set({isUsersLoading: true});
+        try {
+            const response = await axiosInstance.get("/messages/contacts");
+            set({allContacts: response.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({isUsersLoading: false});
+        }
+    },
+
+    getChatPartners: async() => {
+        set({isUsersLoading: true});
+        try {
+            const response = await axiosInstance.get("/messages/chats");
+            set({chats: response.data});
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({isUsersLoading: false});
+        }
+    }
+
+
+}))
